bce01d3377c43e078f3e9318ffa44aeea1decfe4
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-01-08 15:56:29 +0100

    First commit

---
+++ webapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from personas.models import Persona
+
+# Create your views here.
+
+def bienvenido(request):
+
+	no_personas = Persona.objects.count()
+	personas = Persona.objects.all()
+
+	return render(request, 'bienvenido.html', {'no_personas': no_personas, 'personas': personas})
+
+
+
+def despedirse(request):
+	return HttpResponse('Adios')
